309534868695b2c28742c92cad3d31e313f2471b
Ghazanfarijaz <i200870@nu.edu.pk>
2025-09-10 23:42:23 +0500

    deployed version

--- src/api/api.js
+++ src/api/api.js
@@ -7,7 +7,7 @@ const API_BASE_URL =
   import.meta.env &&
   import.meta.env.VITE_API_URL
     ? import.meta.env.VITE_API_URL
-    : "http://localhost:5000/api";
+    : "https://konnectd-vid-admin-backend.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
